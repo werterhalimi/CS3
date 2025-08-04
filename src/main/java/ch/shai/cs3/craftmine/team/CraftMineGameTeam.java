b0da0f1077d7e20423de2632f22740b0a68af0bd
Shaï Halimi <shai.halimi@infomaniak.com>
2025-08-05 00:22:04 +0200

    feat: add exitPortalCenter props to CraftMineGameTeam

--- src/main/java/ch/shai/cs3/craftmine/team/CraftMineGameTeam.java
+++ src/main/java/ch/shai/cs3/craftmine/team/CraftMineGameTeam.java
@@ -1,20 +1,25 @@
 package ch.shai.cs3.craftmine.team;
 
-import ch.shai.cs3.craftmine.player.CraftMineGamePlayer;
-import ch.shai.cs3.game.player.GamePlayer;
 import ch.shai.cs3.game.team.GameTeam;
 import org.bukkit.DyeColor;
 import org.bukkit.Location;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class CraftMineGameTeam extends GameTeam {
     private static final int TEAM_SIZE = 1;
+
+    private Location exitPortalCenter;
 
     public CraftMineGameTeam(String name, DyeColor color, Location spawn) {
         super(new ArrayList<>(), name, color, spawn, CraftMineGameTeam.TEAM_SIZE);
     }
 
+    public void setExitPortalCenter(Location newVal){
+        this.exitPortalCenter = newVal;
+    }
 
+    public Location getExitPortalCenter(){
+        return this.exitPortalCenter;
+    }
 }
