68ce2596b972c4b1aa7593f6629f1399cf94ae35
Shaï Halimi <shai.halimi@infomaniak.com>
2025-08-23 18:15:01 +0200

    feat: add WorldGenerationState

--- src/main/java/ch/shai/cs3/craftmine/CraftMineGame.java
+++ src/main/java/ch/shai/cs3/craftmine/CraftMineGame.java
@@ -2,6 +2,7 @@ package ch.shai.cs3.craftmine;
 
 import ch.shai.cs3.craftmine.player.CraftMineGamePlayer;
 import ch.shai.cs3.craftmine.state.CraftMineGameState;
+import ch.shai.cs3.craftmine.state.WorldGenerationState;
 import ch.shai.cs3.craftmine.team.CraftMineGameTeam;
 import ch.shai.cs3.game.Game;
 import ch.shai.cs3.game.state.sample.hub.HubGameState;
@@ -13,6 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CraftMineGame extends Game<CraftMineGamePlayer, CraftMineGameTeam> {
     public CraftMineGame(JavaPlugin plugin) {
         super(plugin);
+        this.addState(new WorldGenerationState());
         this.addState(new HubGameState<CraftMineGamePlayer, CraftMineGameTeam>(this.getWorld().getSpawnLocation().add(0,40,0),new HubGameStateOption()));
         this.addState(new CraftMineGameState());
         this.teams.add(new CraftMineGameTeam("Les verts !", DyeColor.GREEN,this.getWorld().getSpawnLocation() ));
