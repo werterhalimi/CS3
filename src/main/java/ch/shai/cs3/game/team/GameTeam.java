679dc6aadff1af6d03e67535bcf6751150e71f07
Shaï Halimi <shai.halimi@infomaniak.com>
2025-08-23 18:13:13 +0200

    feat: set list color

--- src/main/java/ch/shai/cs3/game/team/GameTeam.java
+++ src/main/java/ch/shai/cs3/game/team/GameTeam.java
@@ -1,6 +1,9 @@
 package ch.shai.cs3.game.team;
 
 import ch.shai.cs3.game.player.GamePlayer;
+import ch.shai.cs3.utils.itemstack.ColorUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.DyeColor;
 import org.bukkit.Location;
 
@@ -11,6 +14,7 @@ public abstract class GameTeam {
     protected int size;
     protected String name;
     protected DyeColor color;
+    protected NamedTextColor namedTextColor;
     protected Location spawn;
 
     public GameTeam(List<GamePlayer> players, String name, DyeColor color, Location spawn,int size) {
@@ -19,13 +23,21 @@ public abstract class GameTeam {
         this.color = color;
         this.spawn = spawn;
         this.size = size;
+        this.namedTextColor = ColorUtils.dyeColorToChatColor(this.getColor());
     }
 
     public String getName() {
         return name;
     }
+    public NamedTextColor getNamedTextColor() {
+        return namedTextColor;
+    }
     public DyeColor getColor() {
         return color;
+    }
+    public void setColor(DyeColor color) {
+        this.color = color;
+        this.namedTextColor = ColorUtils.dyeColorToChatColor(this.getColor());
     }
 
     public Location getSpawnPoint(){
@@ -50,11 +62,10 @@ public abstract class GameTeam {
     public void addPlayer(GamePlayer player){
         this.players.add(player);
         player.getBukkitPlayer().setRespawnLocation(this.spawn);
+        player.getBukkitPlayer().playerListName(Component.text(player.getBukkitPlayer().getName(), this.getNamedTextColor()));
     }
     public boolean isFull(){
-
         return this.players.size() >= this.size;
-
     }
 
     public List<GamePlayer> getPlayers() {
